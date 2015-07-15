e53d8c08063bfd8b26902c694ecbf8cb93cc21b0
Softball123 <Softball123@users.noreply.github.com>
2015-07-15 13:50:14 -0400

    Create scripy.js

---
+++ scripy.js
@@ -0,0 +1,14 @@
+function popup()  {
+	  open("BATMAN.jpg");
+}
+
+function changeImage()  {
+	var image = document.getElementById(
+	'my image');
+	if (img.src.match("bulb_on.jpg"))  {
+	 image.src = "bulb_off.jpg";
+	}
+  else {
+
+  }
+}  
